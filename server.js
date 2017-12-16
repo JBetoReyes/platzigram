bb64ec82fac6f4b911ed6e8d718e811caa4b1ce2
Jose <jose@localhost.localdomain>
2017-12-16 18:32:19 -0500

    Type Scope Description 1. First commit PR:master

---
+++ server.js
@@ -0,0 +1,15 @@
+var express = require('express');
+var app = express();
+
+app.get('/', function(req, res){
+    res.send('Hello world');
+});
+
+app.listen(3000, function(err){
+    if(err){
+        console.log(err);
+        process.exit(1);
+    }
+
+    console.log('connected in port: 3000')
+});
