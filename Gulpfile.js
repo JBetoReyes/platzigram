0da92be7ba5057b8ce26109f702ecd193ad09a82
JBetoReyes <josereyes55@hotmail.com>
2017-12-16 21:15:46 -0500

    script task added

--- Gulpfile.js
+++ Gulpfile.js
@@ -1,6 +1,9 @@
 var gulp = require('gulp');
 var sass = require('gulp-sass');
 var rename = require('gulp-rename');
+var babel = require('babelify');
+var browserify = require('browserify');
+var source = require('vinyl-source-stream');
 
 // compiles index.scss
 gulp.task('styles',function(){
@@ -15,6 +18,18 @@ gulp.task('assets',function(){
     gulp
         .src('assets/*') //glob
         .pipe(gulp.dest('public'));
-})
+});
 
-gulp.task('default',['styles','assets']);
+gulp.task('scripts',function(){
+        browserify('./src/index.js')
+        .transform(babel)
+        .bundle()
+        .pipe(source('index.js')) //help us to pipe bundle to gulp
+        .pipe(rename('app.js'))
+        .pipe(gulp.dest('public'));
+    }
+);
+
+gulp.task('default',['styles','assets','scripts']);
+
+
