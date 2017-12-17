781f742b35dfee4785d45c78447f586352f8bef9
JBetoReyes <josereyes55@hotmail.com>
2017-12-17 18:11:05 -0500

    adding moment and cards

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -1,8 +1,32 @@
 var page = require('page');
+var empty = require('empty-element');
+var template = require('./template');
 var title = require('title');
 
 page('/', function(ctx, next){
     title('Platzigram');
-    var main = document.getElementById('main-container');   
-    main.innerHTML = '<a href="/signup">Signup</a>'
+    var main = document.getElementById('main-container');
+    var pictures = [
+        {
+            user: {
+                username: 'Jose',
+                avatar: 'https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/14590444_10211153418900784_3682573500301961244_n.jpg?oh=2e86d1543be1778e5987e3194abeac6a&oe=5AB688B3'
+            },
+            url: 'office.jpg',
+            likes:10,
+            liked: true,
+            createdAt: new Date()
+        }, {
+            user: {
+                username: 'Jose',
+                avatar: 'https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/14590444_10211153418900784_3682573500301961244_n.jpg?oh=2e86d1543be1778e5987e3194abeac6a&oe=5AB688B3'
+            },
+            url: 'office.jpg',
+            likes: 2,
+            liked: true,
+            createdAt: new Date().setDate(new Date().getDate() - 10)
+        }        
+    ];    
+    empty(main).appendChild(template(pictures));
+
 });
--- src/index.js
+++ src/index.js
@@ -1,4 +1,7 @@
 var page = require('page'); // router tool module
+var moment = require('moment');
+require('moment/locale/es');
+moment.locale('es');
 require('./homepage');
 require('./signup');
 require('./signin');
