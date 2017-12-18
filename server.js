37b318b00da93399a86997073dca3f15f2c7346e
JBetoReyes <josereyes55@hotmail.com>
2017-12-17 21:59:36 -0500

    adding super agent

--- server.js
+++ server.js
@@ -22,6 +22,33 @@ app.get('/signin', function(req, res){
     });
 });
 
+app.get('/api/pictures', function(req, res, next){
+    var pictures = [
+        {
+            user: {
+                username: 'Jose',
+                avatar: 'https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/14590444_10211153418900784_3682573500301961244_n.jpg?oh=2e86d1543be1778e5987e3194abeac6a&oe=5AB688B3'
+            },
+            url: 'office.jpg',
+            likes:1,
+            liked: true,
+            createdAt: new Date()
+        }, {
+            user: {
+                username: 'Jose',
+                avatar: 'https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/14590444_10211153418900784_3682573500301961244_n.jpg?oh=2e86d1543be1778e5987e3194abeac6a&oe=5AB688B3'
+            },
+            url: 'office.jpg',
+            likes: 2,
+            liked: true,
+            createdAt: new Date(new Date().setDate(new Date().getDate() - 10))
+        }        
+    ];
+    setTimeout(function(){
+        res.send(pictures);
+    },2000);
+});
+
 app.listen(3000, function(err){
     if(err){
         console.log(err);
--- src/homepage/index.js
+++ src/homepage/index.js
@@ -2,31 +2,29 @@ var page = require('page');
 var empty = require('empty-element');
 var template = require('./template');
 var title = require('title');
+var request = require('superagent');
+var header = require('../header');
 
-page('/', function(ctx, next){
+page('/',header, loadPictures, function(ctx, next){
     title('Platzigram');
     var main = document.getElementById('main-container');
-    var pictures = [
-        {
-            user: {
-                username: 'Jose',
-                avatar: 'https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/14590444_10211153418900784_3682573500301961244_n.jpg?oh=2e86d1543be1778e5987e3194abeac6a&oe=5AB688B3'
-            },
-            url: 'office.jpg',
-            likes:1,
-            liked: true,
-            createdAt: new Date()
-        }, {
-            user: {
-                username: 'Jose',
-                avatar: 'https://scontent.fntr6-1.fna.fbcdn.net/v/t1.0-9/14590444_10211153418900784_3682573500301961244_n.jpg?oh=2e86d1543be1778e5987e3194abeac6a&oe=5AB688B3'
-            },
-            url: 'office.jpg',
-            likes: 2,
-            liked: true,
-            createdAt: new Date().setDate(new Date().getDate() - 10)
-        }        
-    ];    
-    empty(main).appendChild(template(pictures));
 
+    empty(main).appendChild(template(ctx.pictures));
 });
+
+function loadPictures(ctx, next){
+    request
+        .get('/api/pictures')
+        .end(function(err, res){
+            if(err){
+                return console.log(err);
+            }
+            ctx.pictures = res.body.map(function(picture){
+                picture.createdAt = new Date(picture.createdAt);
+                return picture;
+            });
+
+            debugger;
+            next();
+        });
+};
