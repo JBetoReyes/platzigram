9254cd0a5fc6c114a45fb6fe5e08cd95874952e7
JBetoReyes <josereyes55@hotmail.com>
2017-12-17 22:13:40 -0500

    Using axios instead of superagent

--- src/homepage/index.js
+++ src/homepage/index.js
@@ -4,8 +4,9 @@ var template = require('./template');
 var title = require('title');
 var request = require('superagent');
 var header = require('../header');
+var axios = require('axios');
 
-page('/',header, loadPictures, function(ctx, next){
+page('/',header, loadPicturesAxios, function(ctx, next){
     title('Platzigram');
     var main = document.getElementById('main-container');
 
@@ -23,8 +24,23 @@ function loadPictures(ctx, next){
                 picture.createdAt = new Date(picture.createdAt);
                 return picture;
             });
-
-            debugger;
             next();
+        });
+};
+
+function loadPicturesAxios(ctx, next){
+    request
+        .get('/api/pictures')
+        .then(function(res){
+            ctx.pictures = res.body.map(function(picture){
+                picture.createdAt = new Date(picture.createdAt);
+                return picture;
+            });
+            next();
+        })
+        .catch(function(err){
+            if(err){
+                console.log(err);
+            }
         });
 };
